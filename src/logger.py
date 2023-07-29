bb6ec86bea10dd3c8a437ba962f61554a9fced82
isdas8 <isdas8@gmail.com>
2023-07-29 18:39:08 +0530

    Logging & Exception

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+# Any execution should be logged 
+import logging 
+import os
+from datetime import datetime 
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" # Creation of file in a format
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE) # path for log file
+os.makedirs (logs_path, exist_ok = True) # Keep appending log files to the folder
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
